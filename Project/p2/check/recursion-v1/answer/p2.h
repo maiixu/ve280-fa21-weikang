527a78a27500595fe2f7e7ec8400c4f2587722fe
xumai07 <xumai07@sjtu.edu.cn>
2021-10-15 18:04:06 +0800

    p2 joj passed

--- Project/p2/check/recursion-v1/answer/p2.h
+++ Project/p2/check/recursion-v1/answer/p2.h
@@ -3,19 +3,19 @@
 #include "recursive.h"
 
 int size(list_t list);
-/* 
+/*
 // EFFECTS: Returns the number of elements in "list".
 //          Returns zero if "list" is empty.
-*/ 
+*/
 
 int sum(list_t list);
-/* 
+/*
 // EFFECTS: Returns the sum of each element in "list".
 //          Returns zero if "list" is empty.
-*/ 
+*/
 
 int product(list_t list);
-/* 
+/*
 // EFFECTS: Returns the product of each element in "list".
 //          Returns one if "list" is empty.
 */
@@ -25,7 +25,7 @@ int accumulate(list_t list, int (*fn)(int, int), int base);
 // REQUIRES: "fn" must be associative.
 //
 // EFFECTS: Returns "base" if "list" is empty.
-//          Returns fn(list_first(list), 
+//          Returns fn(list_first(list),
 //                      accumulate(list_rest(list), fn, base) otherwise.
 //
 // For example, if you have the following function:
@@ -35,7 +35,7 @@ int accumulate(list_t list, int (*fn)(int, int), int base);
 // Then the following invocation returns the sum of all elements:
 //
 //           accumulate(list, add, 0);
-*/ 
+*/
 
 list_t reverse(list_t list);
 /*
@@ -50,22 +50,22 @@ list_t append(list_t first, list_t second);
 //
 // For example: append(( 2 4 6 ), ( 1 3 )) gives
 // the list ( 2 4 6 1 3 ).
-*/ 
+*/
 
 list_t filter_odd(list_t list);
 /*
 // EFFECTS: Returns a new list containing only the elements of the
-//          original "list" which are odd in value, 
+//          original "list" which are odd in value,
 //          in the order in which they appeared in list.
 //
 // For example, if you applied filter_odd to the list ( 3 4 1 5 6 )
 // you would get the list ( 3 1 5 ).
-*/ 
+*/
 
 list_t filter_even(list_t list);
 /*
 // EFFECTS: Returns a new list containing only the elements of the
-//          original "list" which are even in value, 
+//          original "list" which are even in value,
 //          in the order in which they appeared in list.
 */
 
@@ -108,7 +108,7 @@ list_t chop(list_t list, unsigned int n);
 
 int tree_sum(tree_t tree);
 /*
-// EFFECTS: Returns the sum of all elements in "tree". 
+// EFFECTS: Returns the sum of all elements in "tree".
 //          Returns zero if "tree" is empty.
 */
 
@@ -125,7 +125,7 @@ int depth(tree_t tree);
 //          Returns zero is "tree" is empty.
 //
 // For example, the tree
-// 
+//
 //                           4
 //                         /   \
 //                        /      \
@@ -151,10 +151,10 @@ int tree_min(tree_t tree);
 */
 
 list_t traversal(tree_t tree);
-/* 
+/*
 // EFFECTS: Returns the elements of "tree" in a list using an
-//          in-order traversal. An in-order traversal prints 
-//          the "left most" element first, then the second-left-most, 
+//          in-order traversal. An in-order traversal prints
+//          the "left most" element first, then the second-left-most,
 //          and so on, until the right-most element is printed.
 //
 //          For any root element, all the elements of its left subtree
@@ -163,30 +163,30 @@ list_t traversal(tree_t tree);
 //          on the right of that root element.
 //
 // For example, the tree:
-// 
+//
 //                           4
 //                         /   \
 //                        /      \
 //                       2        5
 //                      / \      / \
-//                         3 
+//                         3
 //                        / \
 //
 // would return the list
 //
 //       ( 2 3 4 5 )
-// 
+//
 // An empty tree would print as:
-// 
+//
 //       ( )
 //
 */
 
 bool tree_hasPathSum(tree_t tree, int sum);
-/* 
+/*
 // EFFECTS: Returns true if and only if "tree" has at least one root-to-leaf
 //          path such that adding all elements along the path equals "sum".
-//          
+//
 // A root-to-leaf path is a sequence of elements in a tree starting
 // with the root element and proceeding downward to a leaf (an element
 // with no children).
@@ -194,38 +194,38 @@ bool tree_hasPathSum(tree_t tree, int sum);
 // An empty tree has no root-to-leaf path.
 //
 // For example, the tree:
-// 
+//
 //                           4
 //                         /   \
 //                        /     \
 //                       1       5
 //                      / \     / \
-//                     3   6 
+//                     3   6
 //                    / \ / \
 //
 // has three root-to-leaf paths: 4->1->3, 4->1->6 and 4->5.
 // Given sum = 9, the path 4->5 has the sum 9, so the function
 // should return true. If sum = 10, since no path has the sum 10,
 // the function should return false.
-// 
+//
 */
 
 bool covered_by(tree_t A, tree_t B);
 /*
 // EFFECTS: Returns true if tree A is covered by tree B.
-*/       
+*/
 
 bool contained_by(tree_t A, tree_t B);
 /*
 // EFFECTS: Returns true if tree A is covered by tree B
 //          or any complete subtree of B.
-*/       
+*/
 
 tree_t insert_tree(int elt, tree_t tree);
-/* 
+/*
 // REQUIRES: "tree" is a sorted binary tree.
 //
-// EFFECTS: Returns a new tree with elt inserted at a leaf such that 
+// EFFECTS: Returns a new tree with elt inserted at a leaf such that
 //          the resulting tree is also a sorted binary tree.
 //
 // For example, inserting 1 into the tree:
@@ -235,7 +235,7 @@ tree_t insert_tree(int elt, tree_t tree);
 //                        /      \
 //                       2        5
 //                      / \      / \
-//                         3 
+//                         3
 //                        / \
 //
 // would yield
@@ -244,9 +244,9 @@ tree_t insert_tree(int elt, tree_t tree);
 //                        /      \
 //                       2        5
 //                      / \      / \
-//                     1   3 
+//                     1   3
 //                    / \ / \
-// 
+//
 */
 
 #endif /* __P2_H__ */
